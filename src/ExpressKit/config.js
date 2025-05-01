73adea8ebcb9169e481e1e5cee08dcac5225ba39
2minRain <1207826398@qq.com>
2025-05-01 21:24:12 +0800

    Init

--- src/ExpressKit/config.js
+++ src/ExpressKit/config.js
@@ -1,4 +1,4 @@
-export default config = {
+export default {
     appCode: "d12e7c991128484b93afc13e7ff9b2b2",
     //AppCode: 你的阿里云Kuaidi100 API AppCode.
     resultv2: "4"
--- src/ExpressKit/express.js
+++ src/ExpressKit/express.js
@@ -1,5 +1,6 @@
 import fetch from "@system.fetch";
 import config from "./config";
+import prompt from "@system.prompt";
 
 export async function getExpressInfo({
     com,
@@ -13,9 +14,10 @@ export async function getExpressInfo({
         'Content-Type': 'application/json',
         'Authorization': 'APPCODE ' + config.appCode
     };
-    if(com=="shunfeng" | com=="sf"){
-        throw new Error("顺丰速运需要填写手机号才可查询")
-        return;
+    if(com === "shunfeng" || com === "sf"){
+        if(phone==""){
+            throw new Error("顺丰速运需要填写手机号才可查询")
+        }
     };
     const Data = {
         com: com,
@@ -28,15 +30,16 @@ export async function getExpressInfo({
         resultv2: config.resultv2
     };
     const querys = 'param=%7B%22com%22%3A%22{com}%22%2C%22num%22%3A%22{num}%22%2C%22from%22%3A%22{from}%22%2C%22phone%22%3A%22{phone}%22%2C%22to%22%3A%22{to}%22%2C%22resultv2%22%3A{resultv2}%2C%22show%22%3A%22{show}%22%2C%22order%22%3A%22{order}%22%7D';
-    querys.replace('{com}', com);
-    querys.replace('{num}', num);
-    querys.replace('{from}', from);
-    querys.replace('{phone}', phone);
-    querys.replace('{to}', to);
-    querys.replace('{resultv2}', config.resultv2);
-    querys.replace('{show}', show);
-    querys.replace('{order}', order)
-    const url = "https://kdapi.kuaidi100.com/test/poll/channelquery.do?" + querys;
+    let queryStr = querys
+    .replace('{com}', com)
+    .replace('{num}', num)
+    .replace('{from}', from)
+    .replace('{phone}', phone)
+    .replace('{to}', to)
+    .replace('{resultv2}', config.resultv2)
+    .replace('{show}', show)
+    .replace('{order}', order);
+    const url = "https://kdapi.kuaidi100.com/test/poll/channelquery.do?" + queryStr;
     const result = {};
     await fetch.fetch({
         url: url,
@@ -48,6 +51,12 @@ export async function getExpressInfo({
             console.log(`Response code: ${response.code}`);
             console.log(`Response data: ${JSON.stringify(response.data)}`)
             result = response.data;
+            if(response.code === 200){
+                prompt.showToast({
+                    message: response.data,
+                    duration: 1000
+                })
+            }
         },
         fail: function(data, code) {
             console.log(`handling fail, errMsg = ${data}`)
