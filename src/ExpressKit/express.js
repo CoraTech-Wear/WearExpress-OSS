ca1889f435119c947252fccded62e6b52872b346
liuyuze61 <liuyuze61@163.com>
2025-05-05 14:29:22 +0800

    优化

--- src/ExpressKit/express.js
+++ src/ExpressKit/express.js
@@ -31,6 +31,7 @@ async function getExpressInfo({
         show: "0",
         resultv2: config.resultv2
     };
+    const querys1 = `param=%7B%22com%22%3A%22${com}%22%2C%22num%22%3A%22${num}%22%2C%22from%22%3A%22${from}%22%2C%22phone%22%3A%22${phone}%22%2C%22to%22%3A%22${to}%22%2C%22resultv2%22%3A${resultv2}%2C%22show%22%3A%22${show}%22%2C%22order%22%3A%22${order}%22%7D`;
     const querys = 'param=%7B%22com%22%3A%22{com}%22%2C%22num%22%3A%22{num}%22%2C%22from%22%3A%22{from}%22%2C%22phone%22%3A%22{phone}%22%2C%22to%22%3A%22{to}%22%2C%22resultv2%22%3A{resultv2}%2C%22show%22%3A%22{show}%22%2C%22order%22%3A%22{order}%22%7D';
     let queryStr = querys
     .replace('{com}', com)
